33cb5de2cb3038993267f4c04d203030f81fb4c4
cvBesdelnik <cvbaarsik@gmail.com>
2022-11-15 15:48:18 +0200

    1 task pluss

--- first.rb
+++ first.rb
@@ -1,2 +1,10 @@
 puts ('Hello World!')
 
+# puts "enter width"
+# width = gets.chomp.to_f
+# puts "enter length"
+# length = gets.chomp.to_f
+  
+# perim = 2 * (width + length)
+# puts " We have perrimeter #{perim}"
+
